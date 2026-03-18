3bc012c165dfa7542482c7d727de21eeab7b0d44
AkioHub <akiomidnight23@gmail.com>
2026-03-19 01:45:41 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,97 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+/*
+	* Create By Naze
+	* Follow https://github.com/nazedev
+	* 
+*/
+
+//~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
+
+global.owner = ["6285785247918"] // ['628','628'] 2 owner atau lebih
+global.author = 'AkioHub'
+global.botname = 'Violet Bot'
+global.packname = 'Violet Hub'
+global.timezone = 'Asia/Jakarta' // Ganti pakai command .settimezone Makasar
+global.locale = 'en' // Ganti pakai command .setlocale
+global.listprefix = ['+','!','.']
+
+global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
+global.tempatDB = 'database.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
+global.tempatStore = 'baileys_store.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
+global.pairing_code = true
+global.number_bot = '' // Kalo pake panel bisa masukin nomer di sini, jika belum ambil session. Format : '628xx'
+
+global.fake = {
+	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
+	thumbnailUrl: 'https://telegra.ph/file/fe4843a1261fc414542c4.jpg',
+	thumbnail: fs.readFileSync('./src/media/naze.png'),
+	docs: fs.readFileSync('./src/media/fake.pdf'),
+	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
+}
+
+global.my = {
+	yt: '-',
+	gh: '-',
+	gc: '-',
+	ch: '-',
+}
+
+global.limit = {
+	free: 20,
+	premium: 999,
+	vip: 900
+}
+
+global.money = {
+	free: 10000,
+	premium: 1000000,
+	vip: 10000000
+}
+
+global.mess = {
+	key: "Apikey limit! Silahkan Upgrade: https://naze.biz.id",
+	owner: "Khusus Owner!",
+	admin: "Khusus Admin!",
+	botAdmin: "Bot harus Admin!",
+	onWa: "Nomor tersebut tidak terdaftar di WhatsApp!",
+	group: "Khusus Grup!",
+	private: "Khusus Private Chat!",
+	quoted: "Reply pesannya!",
+	limit: "Limit habis!",
+	prem: "Khusus Premium!",
+	text: "Masukkan teksnya!",
+	media: "Kirim medianya!",
+	wait: "Proses...",
+	fail: "Gagal!",
+	error: "Error!",
+	done: "Selesai!"
+}
+
+global.APIs = {
+	naze: 'https://api.naze.biz.id',
+}
+global.APIKeys = {
+	'https://api.naze.biz.id': 'nz-514094fb33',
+}
+
+// Lainnya
+global.jadwalSholat = {
+	Subuh: '04:30',
+	Dzuhur: '12:06',
+	Ashar: '15:21',
+	Maghrib: '18:08',
+	Isya: '19:00'
+}
+
+global.badWords = ['dongo'] // input kata-kata toxic yg lain. ex: ['dongo','dongonya']
+global.chatLength = 500
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.yellowBright(`[UPDATE] ${__filename}`))
+	delete require.cache[file]
+	require(file)
+});
